3f6364bb693852207da1fcc9a56ed391810a4701
Mateus Freira <mateus.freira@gmail.com>
2014-12-08 14:01:35 -0200

    Pagination]

--- assets/app/controllers/UnitiController.js
+++ assets/app/controllers/UnitiController.js
@@ -1,7 +1,7 @@
 PortalApp.controller('UnitController', ['$scope', '$http', function ($scope, $http) {
 
-	var buildRows = function(data, nIntens){
-		//nIntens
+	var buildRows = function(data, nIntens, nRowToPage){
+		var pages = [];
 		var rows = [];
 		var row = [];
 		data.forEach(function(item, i){
@@ -10,14 +10,43 @@ PortalApp.controller('UnitController', ['$scope', '$http', function ($scope, $ht
 					rows.push(row);
 					row = [];
 				}
+				if(rows.length === nRowToPage){
+					pages.push({ rows: rows});
+					rows = [];
+				}
 			}
 			row.push(item);
 		});
-		rows.push(row);
-		return rows;
+		if(row.length){
+			rows.push(row);
+		}
+		if(rows.length){
+			pages.push({ rows : rows});
+		}
+		return pages;
 	};
 
 	$http.get(PortalApp.serviceUrl+"/unities").then(function(repose){
-		$scope.rows = buildRows(PortalApp.parseRequest(repose.data), 4);
+		$scope.pages = buildRows(PortalApp.parseRequest(repose.data), 4, 1);
+		$scope.page = $scope.pages[0];
 	});
+	$scope.chagePage = function(page){
+		$scope.page = page;
+	};
+	$scope.nextPage = function(){
+		var currentIndex = $scope.pages.indexOf($scope.page)-1;
+		currentIndex++;
+		if(currentIndex === $scope.pages.length){
+			currentIndex = 0;			
+		}
+		$scope.page = $scope.pages[currentIndex];
+	};	
+	$scope.prevPage = function(){
+		var currentIndex = $scope.pages.indexOf($scope.page)-1;
+		currentIndex--;
+		if(currentIndex === -1){
+			currentIndex = $scope.pages.length;
+		}
+		$scope.page = $scope.pages[currentIndex];
+	};		
 }]);
