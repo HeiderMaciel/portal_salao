882d7a21059ace4e84cd82fdb63a09c1a9421eca
Mateus Freira <mateus.freira@gmail.com>
2015-02-12 00:53:10 -0200

    Vagas no portal

---
+++ assets/app/controllers/JobRequest.js
@@ -0,0 +1,52 @@
+PortalApp.controller('JobRequestController', ['$scope', '$http', function ($scope, $http) {
+
+	var buildRows = function(data, nIntens, nRowToPage){
+		var pages = [];
+		var rows = [];
+		var row = [];
+		data.forEach(function(item, i){
+			if(i % nIntens === 0){
+				if(row.length > 0){
+					rows.push(row);
+					row = [];
+				}
+				if(rows.length === nRowToPage){
+					pages.push({ rows: rows});
+					rows = [];
+				}
+			}
+			row.push(item);
+		});
+		if(row.length){
+			rows.push(row);
+		}
+		if(rows.length){
+			pages.push({ rows : rows});
+		}
+		return pages;
+	};
+
+	$http.get(PortalApp.serviceUrl+"/job_requisitions").then(function(repose){
+		$scope.pages = buildRows(PortalApp.parseRequest(repose.data), 4, 3);
+		$scope.page = $scope.pages[0];
+	});
+	$scope.chagePage = function(page){
+		$scope.page = page;
+	};
+	$scope.nextPage = function(){
+		var currentIndex = $scope.pages.indexOf($scope.page);
+		currentIndex++;
+		if(currentIndex === $scope.pages.length){
+			currentIndex = 0;			
+		}
+		$scope.page = $scope.pages[currentIndex];
+	};	
+	$scope.prevPage = function(){
+		var currentIndex = $scope.pages.indexOf($scope.page);
+		currentIndex--;
+		if(currentIndex === -1){
+			currentIndex = $scope.pages.length;
+		}
+		$scope.page = $scope.pages[currentIndex];
+	};		
+}]);
--- assets/dist/main.js
+++ assets/dist/main.js
@@ -281,6 +281,60 @@ PortalApp.parseRequest = function(data) {
 PortalApp.serviceUrl = "http://ebelle.vilarika.com.br/site";//production
 //PortalApp.serviceUrl = "http://localhost:7171/site";//local
 
+// FILE: app/controllers/JobRequest.js
+PortalApp.controller('JobRequestController', ['$scope', '$http', function ($scope, $http) {
+
+	var buildRows = function(data, nIntens, nRowToPage){
+		var pages = [];
+		var rows = [];
+		var row = [];
+		data.forEach(function(item, i){
+			if(i % nIntens === 0){
+				if(row.length > 0){
+					rows.push(row);
+					row = [];
+				}
+				if(rows.length === nRowToPage){
+					pages.push({ rows: rows});
+					rows = [];
+				}
+			}
+			row.push(item);
+		});
+		if(row.length){
+			rows.push(row);
+		}
+		if(rows.length){
+			pages.push({ rows : rows});
+		}
+		return pages;
+	};
+
+	$http.get(PortalApp.serviceUrl+"/job_requisitions").then(function(repose){
+		$scope.pages = buildRows(PortalApp.parseRequest(repose.data), 4, 3);
+		$scope.page = $scope.pages[0];
+	});
+	$scope.chagePage = function(page){
+		$scope.page = page;
+	};
+	$scope.nextPage = function(){
+		var currentIndex = $scope.pages.indexOf($scope.page);
+		currentIndex++;
+		if(currentIndex === $scope.pages.length){
+			currentIndex = 0;			
+		}
+		$scope.page = $scope.pages[currentIndex];
+	};	
+	$scope.prevPage = function(){
+		var currentIndex = $scope.pages.indexOf($scope.page);
+		currentIndex--;
+		if(currentIndex === -1){
+			currentIndex = $scope.pages.length;
+		}
+		$scope.page = $scope.pages[currentIndex];
+	};		
+}]);
+
 // FILE: app/controllers/PageUnitController.js
 PortalApp.controller('PageUnitController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
 
