9977769deb8898479e6e833cd35d20b7da70e3af
Heider <hrmaciel@gmail.com>
2017-04-11 14:50:33 -0300

    Voltando para localhost

--- assets/dist/main.js
+++ assets/dist/main.js
@@ -285,7 +285,7 @@ PortalApp.getLovalVar = function(name) {
 };
 
 PortalApp.serviceUrl = "https://ebelle.vilarika.com.br/site";//production
-//PortalApp.serviceUrl = "https://66.228.57.73:7171/site";//local
+//PortalApp.serviceUrl = "https://localhost:7171/site";//local
 
 // FILE: app/controllers/JobRequest.js
 PortalApp.controller('JobRequestController', ['$scope', '$http', function ($scope, $http) {
