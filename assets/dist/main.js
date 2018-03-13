f06b096f4cc51c1528d72f50315f4d94420163ac
rigel <rigel.ferreira@hotmail.com>
2018-03-13 01:03:56 -0300

    joinus duplicate - remember password

--- assets/dist/main.js
+++ assets/dist/main.js
@@ -8618,11 +8618,12 @@ PortalApp.controller('UnitController', ['$scope', '$http', '$location', function
 // FILE: app/controllers/joinus.js
 PortalApp.controller('JoinusController', ['$scope', '$http', function ($scope, $http) {
 	$scope.joinus = function(name, mobilephone, 
-		phone, email, password, password2){
+		phone, email, doc){
 		if (email == undefined || email == "") {
 			alert ("Por favor informe um email!");
 			return
 		}
+/*
 		if (password == undefined || password == "" || password.length < 6) {
 			alert ("Por favor informe uma senha com no mínimo 6 caracteres!")
 			return
@@ -8631,19 +8632,45 @@ PortalApp.controller('JoinusController', ['$scope', '$http', function ($scope, $
 			alert ("Senhas não conferem!")
 			return;
 		}
+*/
+		if ((mobilephone == undefined || mobilephone == "") &&
+		    (phone == undefined || phone == "")) {
+			alert ("Pelo menos um dos telefones precisa ser informado!")
+			return;
+		}
+		if (mobilephone == undefined) {
+			mobilephone = "";
+		}
+		if (phone == undefined) {
+			phone = "";
+		}
+		if (doc == undefined) {
+			doc = "";
+		}
 		var params = "?company="+gup('id')+
 					 "&name="+name+
 					 "&mobilephone="+mobilephone+
 					 "&phone="+phone+
 					 "&email="+email+
-					 "&password="+password;
+					 "&doc="+doc;
 		$http.post(PortalApp.serviceUrl+"/../mobile/api/joinus"+params).then(
 			function(results){
 				// rodar com f12 mostra estrutura do objeto
 				//console.log(results)
 	        if(results.data === 1 || results.data == "1"){
-				alert("Cadastramento efetuado com sucesso!");
-				$scope.login(email, password);
+				alert("Cadastramento efetuado com sucesso!\n\nAcesse o email e clique no link para cadastrar sua senha.");
+		        $http.post(PortalApp.serviceUrl+
+		            "/../security/remember_customer_password", 
+		            {email : email, company : gup('id')}).then(function(results){
+		            if(results.data === 1 || results.data == "1"){
+		                alert("Enviado com sucesso para " + email);
+		            }else{
+		                alert(results.data);
+		            }
+		        });    
+		        // antes informava a senha e fazia o login sem
+		        // confirmação do email
+				// $scope.login(email, password);
 	        }else{
 		        alert(results.data);
 	        }
